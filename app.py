1dc16570f5e3532bd4d62255386354d7e0db32c1
Pudxe <korolpopka@gmail.com>
2022-12-30 10:38:13 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+"""This is a main file to run the application"""
+from flask import Flask, request, abort
+from container import checker, user_request
+# ------------------------------------------------------------------------
+app = Flask(__name__)
+# ------------------------------------------------------------------------
+
+
+@app.route("/perform_query/", methods=['POST'])
+def perform_query():
+    """This function is a view to work with route /perform_query/ with
+    different query parameters
+
+    Returns:
+        flask response with result of the query execution
+    """
+    args = request.values
+
+    if not checker.is_valid(args):
+        abort(400, 'Bad Request, arguments is invalid')
+
+    try:
+        result = user_request.execute(**args)
+
+    except Exception as e:
+        abort(400, f'Bad Request, there is an error {e}')
+
+    if not result:
+        abort(404, 'Nothing was found')
+
+    return app.response_class(result, content_type="text/plain")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ constants.py
@@ -0,0 +1,18 @@
+"""This file contains different constants containing file paths or keys to
+validate"""
+import os
+# --------------------------------------------------------------------------
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "../homework)23/data")
+
+VALID_KEYS = {'cmd1', 'cmd2', 'value1', 'value2', 'file'}
+VALID_VALUES = {'filter', 'map', 'sort', 'unique', 'limit'}
+
+QUERY = {
+    'filter': 'filter',
+    'map': 'map',
+    'unique': 'unique',
+    'sort': 'sort',
+    'limit': 'limit'
+}
